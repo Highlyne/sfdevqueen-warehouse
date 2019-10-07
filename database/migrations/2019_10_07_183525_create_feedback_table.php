843d974def0fc2157f375e1ff48a31bc555dd257
Tamara Hall <thall2000@gmail.com>
2019-10-07 17:53:58 -0400

    adding feedback table to local and heroku

---
+++ database/migrations/2019_10_07_183525_create_feedback_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('acctNum');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('answer1');
+            $table->integer('answer2');
+            $table->integer('answer3');
+            $table->integer('answer4');
+            $table->integer('answer5');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
