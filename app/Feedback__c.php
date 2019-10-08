89a07e6af23d6aaf26dbd2e8e1d3ba43ea6bf706
Tamara Hall <thall2000@gmail.com>
2019-10-08 02:55:53 -0400

    adding var to remove timestamp

--- app/Feedback__c.php
+++ app/Feedback__c.php
@@ -8,7 +8,7 @@ class Feedback__c extends Model
 {
     //
     protected $table = 'feedback__c';
-    
+    public $timestamps = false;
     protected $fillable = [
         'acctNum', 'email', 'name', 'answer1',
         'answer2', 'answer3', 'answer4', 'answer5'
